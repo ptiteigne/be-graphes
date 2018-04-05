48433e85ee59452ad0a8b4ac781c8ff0ddc0334e
Ptiteigne <julien.bonnasserre.1@ens.etsmtl.ca>
2018-04-05 10:37:51 +0200

    Began implementing DijkstraAlgorithm

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,10 @@
 package org.insa.algo.shortestpath;
 
+import java.util.Arrays;
+
+import org.insa.graph.Arc;
+import org.insa.graph.Graph;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -8,9 +13,28 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     @Override
     protected ShortestPathSolution doRun() {
-        ShortestPathData data = getInputData();
-        ShortestPathSolution solution = null;
-        // TODO:
+
+    	// Retrieve the graph.
+   		ShortestPathData data = getInputData();
+    	Graph graph = data.getGraph();        
+        
+        final int nbNodes = graph.size();
+        
+		// Initialize array of labels.
+		Label[] labels = new Label[nbNodes];
+		
+		// Set source cost to zero and insert it on the heap
+		labels[data.getOrigin().getId()].setCost(0);
+
+		// Notify observers about the first event (origin processed).
+		notifyOriginProcessed(data.getOrigin());
+
+		// Initialize array of predecessors.
+		Arc[] predecessorArcs = new Arc[nbNodes];
+        
+        
+		ShortestPathSolution solution = null;
+        
         return solution;
     }
 
