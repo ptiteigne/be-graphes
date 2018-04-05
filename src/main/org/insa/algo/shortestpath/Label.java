31ae9d7218335c2d09e4cf25b2684d12fcfd298b
Ptiteigne <julien.bonnasserre.1@ens.etsmtl.ca>
2018-04-05 10:25:25 +0200

    Began implementing the class Label for the Dijkstra Algorithm

---
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -0,0 +1,39 @@
+package org.insa.algo.shortestpath;
+
+import org.insa.graph.Node;
+
+public class Label {
+	
+	private int cost;
+	
+	private Node father;
+	
+	private boolean isMarked;
+	
+	/**
+	 * Constructor of class Label
+	 * The label is initialized like this :
+	 * <ul>
+     *	<li> the node is NOT marked </li>
+     *	<li> the node has no father (father = null) </li>
+     *	<li> the cost to reach the node is Infinity.MAX_VALUE </li>
+	 * </ul>
+	 * 
+	 */
+	public Label() {
+		this.cost = Integer.MAX_VALUE;
+		this.father = null;
+		this.isMarked = false;
+	}
+	
+	// Get the cost to reach this node
+	public int getCost() {
+		return this.cost;
+	}
+	
+	// Set the cost to reach this node
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+}
